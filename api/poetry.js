85c38dc339383c526e69a4355bda7e1f4c1020ce
I'm a robot <97980752+zoreva@users.noreply.github.com>
2022-02-09 12:24:38 +0300

    update api

--- api/poetry.js
+++ api/poetry.js
@@ -1,71 +1,81 @@
 const PORT = process.env.PORT || 8080;
 const GET_HOST = process.env.GET_HOST || 'http://localhost';
 
-const {divCeil} = require('../utils/divCeil');
-const {nanoid} = require('nanoid');
-const {Client} = require('pg');
+const { divCeil } = require('../utils/divCeil');
+const { nanoid } = require('nanoid');
+const { Client } = require('pg');
 
 const connectDB = function () {
-    return new Client({
-        connectionString: process.env.POSTGRES_URI,
-        ssl: {
-            rejectUnauthorized: false,
-        },
-    });
-}
+	return new Client({
+		connectionString: process.env.POSTGRES_URI,
+		ssl: {
+			rejectUnauthorized: false,
+		},
+	});
+};
 
 /** GET /api/poetry/all */
 console.log(`##poetry ${GET_HOST}:${PORT}/api/poetry/all`);
 exports.getPoetryAll = (req, res) => {
-    const {pageNumber, counterContent} = req.query;
+	const { pageNumber, counterContent } = req.query;
+    const queryGetCountRecords = `SELECT COUNT(1) FROM public.poetry`
+	const queryGetPoetries = `
+        SELECT id, title, poetry_type, content_poetry, create_date, background, book 
+        FROM public.poetry
+        LIMIT ${counterContent}
+        OFFSET ${counterContent * (pageNumber - 1)};
+    `;
 
-    const client = connectDB();
+	const client = connectDB();
 
-    client.connect();
+	client.connect();
 
-    client.query(`
-            SELECT id, title, poetry_type, content_poetry, create_date, background, book 
-            FROM public.poetry
-            LIMIT ${counterContent}
-            OFFSET ${counterContent * (pageNumber - 1)};
-        `,
-        (errorDb, resultDb) => {
+    client.query(queryGetCountRecords, (errorDb, resultDb) => {
+        if (errorDb) {
+			console.log(errorDb);
+			client.end();
+			return res.sendStatus(500);
+		}
+        console.log(resultDb.rows[0].count);
+        return getPortitionData(resultDb.rows[0].count);
+    })
+	
+    function getPortitionData(rowCount) {
+        client.query(queryGetPoetries, (errorDb, resultDb) => {
             if (errorDb) {
                 console.log(errorDb);
                 client.end();
                 return res.sendStatus(500);
-            }
+            }    
             res.send({
-                pageCounter: divCeil(resultDb.rowCount, counterContent),
-                count: resultDb.rowCount,
-                array: resultDb.rows
+                pageCounter: divCeil(rowCount, counterContent),
+                count: rowCount,
+                array: resultDb.rows,
             });
             client.end();
-        },
-    );
-    return;
+        });
+    }
+
+	return;
 };
 
 /** GET /api/poetry/:id */
 console.log(`##poetry ${GET_HOST}:${PORT}/api/poetry/:id`);
 exports.getPoetryById = (req, res) => {
-    const {id} = req.params;
+	const { id } = req.params;
 
-    const client = connectDB();
+	const client = connectDB();
 
-    client.connect();
+	client.connect();
 
-    client.query(
-        `SELECT * FROM public.poetry WHERE id='${id}';`,
-        (errorDb, resultDb) => {
-            if (errorDb) {
-                console.log(errorDb);
-                client.end();
-                return res.sendStatus(500);
-            }
-            res.send(resultDb.rows);
-            client.end();
-        },
-    );
-    return;
+	client.query(`SELECT * FROM public.poetry WHERE id='${id}';`, (errorDb, resultDb) => {
+		if (errorDb) {
+			console.log(errorDb);
+			client.end();
+			return res.sendStatus(500);
+		}
+		res.send(resultDb.rows);
+		client.end();
+	});
+	return;
 };
